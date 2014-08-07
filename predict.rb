77b579ca4442a6943164fd62a6668399f54c05b5
Zoltan Biber <zoltan.biber@gmail.com>
2014-08-07 12:24:26 +0100

    wrote my train of thought as comments

---
+++ predict.rb
@@ -0,0 +1,8 @@
+# represent a Person or Email in an object-oriented approach
+# write four methods that take 'name' and 'domain' arguments and return an email address
+# write unit tests for methods in a TDD approach
+# put the sample dataset into the file to see which company uses which algorithm
+# ask the user to input the advisor's name and company domain
+# call the appropriate methods and return pssible email addresses for that person 
+# e.g. if the domain matches 'google.com', call the two methods/algorithms google is using 
+# in case the domain is not in the sample dataset (e.g. 'whitehouse.gov') return an error message 
