c3718816be27a9cf425277bf323169ba8a7bc905
Zoltan Biber <zoltan.biber@gmail.com>
2014-08-08 16:20:08 +0100

    refactored unit test for person and made it pass

--- lib/person.rb
+++ lib/person.rb
@@ -1,27 +1,3 @@
-# represent a Person or Email in an object-oriented approach
-# write unit tests for methods in a TDD approach
-# put the sample dataset into the file to see which company uses which algorithm
-# ask the user to input the advisor's name and company domain
-# call the appropriate methods and return pssible email addresses for that person 
-# e.g. if the domain matches 'google.com', call the two methods/algorithms google is using 
-# in case the domain is not in the sample dataset (e.g. 'whitehouse.gov') return an error message 
-
-sample_dataset = {
-  "John Ferguson" => "john.ferguson@alphasights.com",
-  "Damon Aw" => "damon.aw@alphasights.com",
-  "Linda Li" => "linda.li@alphasights.com",
-  "Larry Page" => "larry.p@google.com",
-  "Sergey Brin" => "s.brin@google.com",
-  "Steve Jobs" => "s.j@apple.com"
-}
-
-to_predict = {
-  "Peter Wong" => "alphasights.com",
-  "Craig Silverstein" => "google.com",
-  "Steve Wozniak" => "apple.com",
-  "Barack Obama" => "whitehouse.gov"
-}
-
 class Person
   attr_accessor :first_name
   attr_accessor :last_name
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,20 +1,6 @@
 require 'spec_helper'
 
 describe Person do
-  before do
-    @craig = Person.new
-    @craig.first_name = "Craig" 
-    @craig.last_name = "Silverstein" 
-    @craig.domain = "google.com"
-    @steve = Person.new
-    @steve.first_name = "Steve" 
-    @steve.last_name = "Wozniak" 
-    @steve.domain = "apple.com"
-    @barack = Person.new
-    @barack.first_name = "Barack" 
-    @barack.last_name = "Obama" 
-    @barack.domain = "whitehouse.gov" 
-  end
 
   describe "Peter Wong" do
     before do
@@ -23,11 +9,54 @@ describe Person do
       @peter.last_name = "Wong"
       @peter.domain = "alphasights.com"
     end
+
     it "should predict email based on person's name and domain" do
       expect(@peter.predict_email).to eq("peter.wong@alphasights.com")
     end
+
   end
 
+  describe "Craig Silverstein" do
+    before do
+      @craig = Person.new
+      @craig.first_name = "Craig" 
+      @craig.last_name = "Silverstein" 
+      @craig.domain = "google.com"
+    end
+
+    it "should predict email based on person's name and domain" do
+      expect(@craig.predict_email).to eq(["craig.s@google.com", "c.silverstein@google.com"])
+    end
+
+  end
+
+  describe "Steve Wozniak" do
+    before do
+      @steve = Person.new
+      @steve.first_name = "Steve" 
+      @steve.last_name = "Wozniak" 
+      @steve.domain = "apple.com"
+    end
+
+    it "should predict email based on person's name and domain" do
+      expect(@steve.predict_email).to eq("s.w@apple.com")
+    end
+
+  end
+
+  describe "Barack Obama" do
+    before do
+      @barack = Person.new
+      @barack.first_name = "Barack" 
+      @barack.last_name = "Obama" 
+      @barack.domain = "whitehouse.gov" 
+    end
+
+    it "should predict email based on person's name and domain" do
+      expect {@barack.predict_email}.to raise_error("Sorry, we don't have any person with this company domain in our system so we can't efficiently predict their email address.")
+    end
+
+  end
 
 
 end
