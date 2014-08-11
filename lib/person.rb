99734788c593fdbdba1067f7aa28be525ca71370
Zoltan Biber <zoltan.biber@gmail.com>
2014-08-11 13:51:11 +0100

    refactored/simplified predict_email method

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,10 @@
-require 'pry'
 class Person
   attr_accessor :first_name
   attr_accessor :last_name
   attr_accessor :domain
 
   def predict_email
-    person_object = Pattern.for_person(self)
-    if person_object.is_a? Array
-      people = []
-      person_object.each do |person| 
-        people << person.email
-      end
-    return people   
-    else
-      person_object.email
-    end
+    Pattern.for_person(self).map { |pattern| pattern.email }
   end
 
 end
